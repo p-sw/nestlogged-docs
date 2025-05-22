da3deac897dc0edea3f130dd2447d367c018bb4d
Shinwoo PARK <shinwoo.park@psw.kr>
2025-05-23 05:13:41 +0900

    fix: build mdx manually with script

---
+++ scripts/docs-builder.ts
@@ -0,0 +1,25 @@
+import { compile } from "@mdx-js/mdx";
+import { matchesGlob, join } from "path";
+import { readFile, readdir, writeFile } from "fs/promises";
+
+async function main() {
+  const files = (await readdir("src/documentations", { recursive: true }))
+    .filter((fn) => matchesGlob(fn, "**/*.mdx"))
+    .map((fn) => join("src/documentations", fn));
+
+  await Promise.all(
+    files.map(async (fn) => {
+      const compiled = await compile(await readFile(fn), {
+        jsx: true,
+      });
+      await writeFile(
+        fn.split(".").slice(0, -1).join(".") + ".jsx",
+        compiled.value,
+      );
+    }),
+  );
+
+  console.log("done.");
+}
+
+await main();
--- vite.config.js
+++ vite.config.js
@@ -4,12 +4,10 @@ import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 import { resolve } from "node:path";
 import tailwindcss from "@tailwindcss/vite";
 import Icons from "unplugin-icons/vite";
-import mdx from "@mdx-js/rollup";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    { enforce: "pre", ...mdx() },
     TanStackRouterVite({ autoCodeSplitting: true }),
     viteReact(),
     tailwindcss(),

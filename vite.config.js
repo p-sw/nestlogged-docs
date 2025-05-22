8a74177a0244e8a9baf6fad8873aef53d5b47715
Shinwoo PARK <shinwoo.park@psw.kr>
2025-05-22 21:07:38 +0900

    feat: add unplugin-icons

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import viteReact from "@vitejs/plugin-react";
 import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 import { resolve } from "node:path";
 import tailwindcss from "@tailwindcss/vite";
+import Icons from "unplugin-icons/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -10,6 +11,7 @@ export default defineConfig({
     TanStackRouterVite({ autoCodeSplitting: true }),
     viteReact(),
     tailwindcss(),
+    Icons({ compiler: "jsx", jsx: "react" }),
   ],
   test: {
     globals: true,

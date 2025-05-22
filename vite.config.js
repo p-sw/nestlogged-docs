f038cf812eb33adf89e553ad8d794e1b8403f3a2
Shinwoo PARK <shinwoo.park@psw.kr>
2025-05-23 03:53:09 +0900

    feat: add mdx

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,12 @@ import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 import { resolve } from "node:path";
 import tailwindcss from "@tailwindcss/vite";
 import Icons from "unplugin-icons/vite";
+import mdx from "@mdx-js/rollup";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
+    { enforce: "pre", ...mdx() },
     TanStackRouterVite({ autoCodeSplitting: true }),
     viteReact(),
     tailwindcss(),

a0f8500113e1dfa6c98332f5ee7f393aa27d7716
Shinwoo PARK <shinwoo.park@psw.kr>
2025-05-26 14:16:05 +0900

    feat: apply codeblock metadata plugin

--- scripts/docs-builder.ts
+++ scripts/docs-builder.ts
@@ -4,6 +4,7 @@ import withToc from "@stefanprobst/rehype-extract-toc";
 import withTocExport from "@stefanprobst/rehype-extract-toc/mdx";
 import { matchesGlob, join } from "path";
 import { readFile, readdir, writeFile } from "fs/promises";
+import remarkCodeMeta from "./plugins/remark-code-meta.js";
 
 async function main() {
   const files = (await readdir("src/documentations", { recursive: true }))
@@ -14,6 +15,7 @@ async function main() {
     files.map(async (fn) => {
       const compiled = await compile(await readFile(fn), {
         jsx: true,
+        remarkPlugins: [remarkCodeMeta],
         rehypePlugins: [withSlug, withToc, withTocExport],
       });
       await writeFile(
---
+++ scripts/plugins/remark-code-meta.ts
@@ -0,0 +1,116 @@
+import { visit } from "unist-util-visit";
+import type { Node, Parent } from "unist";
+
+interface NewNode extends Node {
+  lang: string | null;
+  meta: string | null;
+}
+interface ParentNode extends Node {
+  children?: ParentNode[];
+  value?: string;
+}
+
+interface RemarkPlugin {
+  (tree: Node): void;
+}
+
+function splitter(str: string): string[] | null {
+  return str.match(/(?:[^\s"']+|("|')[^"']*("|'))+/g);
+}
+
+const languages: Record<string, string> = {
+  ts: "TypeScript",
+  typescript: "TypeScript",
+  log: "Log",
+};
+
+export default function remarkCodeMeta(): RemarkPlugin {
+  function visitor(node: NewNode, index: number, parent: Parent | undefined) {
+    const { lang, meta } = node;
+    if (lang !== null) {
+      node.data = {
+        hProperties: {
+          dataLanguage: lang,
+        },
+      };
+    }
+    if (!!parent && meta !== null) {
+      const splitted = splitter(meta);
+      if (!splitted) return;
+      const params = splitted
+        .filter((s) => s !== "")
+        .map((s) => s.trim())
+        .reduce(
+          (acc, item) => {
+            const index = item.indexOf(":");
+            acc[item.substring(0, index)] = item
+              .substring(index + 1)
+              .replace(/"(.+)"/, "$1")
+              .replace(/'(.+)'/, "$1");
+            return acc;
+          },
+          {} as Record<string, string>,
+        );
+      const replacedNode: ParentNode = {
+        type: "paragraph",
+        data: {
+          hName: "div",
+          hProperties: {
+            className: "remark-code-meta-wrapper",
+          },
+        },
+        children: [],
+      };
+      const metaBlock: ParentNode = {
+        type: "paragraph",
+        data: {
+          hName: "div",
+          hProperties: {
+            className: "remark-code-meta-block",
+          },
+        },
+        children: [],
+      };
+      replacedNode.children!.push(metaBlock, parent.children[index]);
+      if ("file" in params || "title" in params) {
+        metaBlock.children!.push({
+          type: "paragraph",
+          data: {
+            hName: "div",
+            hProperties: {
+              className: "remark-code-meta__filename",
+            },
+          },
+          children: [
+            {
+              type: "text",
+              value: params["file"] ?? params["title"],
+            },
+          ],
+        });
+      }
+      if (lang) {
+        metaBlock.children!.push({
+          type: "paragraph",
+          data: {
+            hName: "div",
+            hProperties: {
+              classNAme: "remark-code-meta__lang",
+            },
+          },
+          children: [
+            {
+              type: "text",
+              value: languages[lang],
+            },
+          ],
+        });
+      }
+      parent.children.splice(index, 1, replacedNode);
+    }
+    node.meta = null;
+  }
+  return function transformer(tree: Node): void {
+    visit<NewNode, "code">(tree as NewNode, "code", visitor);
+  };
+}
